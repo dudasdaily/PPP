61787564d4d04acbed39224ec92062fdddb73184
dudasdaily <66531025+dudasdaily@users.noreply.github.com>
2025-08-20 12:08:37 +0000

    계산기 - 변가추가. TODO: 변수 재할당

--- practice/calculator.cpp
+++ practice/calculator.cpp
@@ -10,19 +10,24 @@ using namespace std;
 const char number = '8';
 const char quit = 'q';
 const char print = ';';
+const char let = 'L';
+const char var_kind = 'a';
 
 const string prompt = "> ";
 const string result = "= ";
+const string declkey = "let";
 
 class Token
 {
 public:
     char kind;
     double value;
+    string name; // 계산기에서 사용할 변수의 이름
 
     Token() {}
     Token(char ch) : kind(ch), value(0) {}
     Token(char ch, double val) : kind(ch), value(val) {}
+    Token(char ch, string var) : kind(ch), name(var) {}
 };
 
 class Token_stream
@@ -34,6 +39,51 @@ public:
     void putback(Token t);
     Token get();
 };
+
+class Variable
+{
+public:
+    string name;
+    double value;
+
+    Variable(string name, double value) : name(name), value(value) {}
+};
+
+vector<Variable> var_table;
+
+bool is_declared(string name)
+{
+    for (const Variable &v : var_table)
+        if (v.name == name)
+            return true;
+
+    return false;
+}
+
+double get_value(string name)
+{
+    for (const Variable &v : var_table)
+        if (name == v.name)
+            return v.value;
+
+    const string err_msg = "var_table에 " + name + "변수가 없습니다.";
+    error(err_msg);
+}
+
+void set_value(string name, double val)
+{
+    for (Variable &v : var_table)
+    {
+        if (v.name == name)
+        {
+            v.value = val;
+            return;
+        }
+    }
+
+    const string err_msg = "var_table에 " + name + "변수가 없습니다.";
+    error(err_msg);
+}
 
 void Token_stream::putback(Token t)
 // 인자로 받은 토큰을 Token_stream의 버퍼에 넣음.
@@ -61,6 +111,8 @@ Token Token_stream::get()
     {
     case quit:
     case print:
+    case let:
+    case '=':
     case '+':
     case '-':
     case '*':
@@ -90,7 +142,25 @@ Token Token_stream::get()
     }
 
     default:
+    {
+        if (isalpha(ch))
+        {
+            string var;
+            var += ch;
+
+            while (cin.get(ch) && (isalpha(ch) || isdigit(ch)))
+                var += ch;
+
+            cin.putback(ch);
+
+            if (var == declkey)
+                return Token(let, var);
+
+            return Token(var_kind, var);
+        }
+
         error("Invalid Token");
+    }
         return -1;
     }
 }
@@ -118,6 +188,15 @@ double primary()
 
     case number:
         return t.value;
+
+    case '-':
+        return -primary();
+
+    case '+':
+        return primary();
+
+    case var_kind:
+        return get_value(t.name);
 
     default:
         error("Invalid Token");
@@ -197,6 +276,51 @@ double expression()
     }
 }
 
+double declaration()
+{
+    Token t = ts.get();
+
+    switch (t.kind)
+    {
+    case var_kind:
+    {
+        if (is_declared(t.name))
+            error(t.name, "이 var_table에 이미 존재합니다. 같은 변수를 두번 선언 할 수 없습니다!");
+
+        string name = t.name;
+
+        t = ts.get();
+
+        if (t.kind != '=')
+            error("expected '='");
+
+        double d = expression();
+
+        var_table.push_back(Variable(name, d));
+
+        return d;
+    }
+
+    default:
+        error("expected variable");
+    }
+}
+
+double statement()
+{
+    Token t = ts.get();
+
+    switch (t.kind)
+    {
+    case let:
+        return declaration();
+
+    default:
+        ts.putback(t);
+        return expression();
+    }
+}
+
 void calculate()
 {
     try
@@ -214,7 +338,7 @@ void calculate()
 
             ts.putback(t);
 
-            cout << result << expression() << '\n';
+            cout << result << statement() << '\n';
         }
     }
     catch (exception &e)
